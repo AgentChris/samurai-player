0a7c29796e72f86c878a7ba6bac2b54bd2afcf33
chris.lego <poputeacristi@gmail.com>
2018-07-17 22:16:44 +0300

    fix gh-pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MusicCard from './components/MusicCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MusicCard
+          title={'The way I am'}
+          artist={'John Mayer'}
+          profilePic={'https://uiaa-web.azureedge.net/wp-content/uploads/2016/08/uiaa-sustainability-intro-1300x600.jpg'}
+          cover_img={'https://ksassets.timeincuk.net/wp/uploads/sites/55/2017/12/John-Mayer-920x584.jpg'}
+          tag={'Rock'}
+          listenCount={'221'}
+        />
       </div>
     );
   }
